8df71ae311effb147f6a16cb3f4ec50c1bd55338
mutombolin <mutombolin@gmail.com>
2014-01-03 17:33:48 +0800

    vlcplayer project

---
+++ vlcplayer/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using AxAXVLC;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace vlcplayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AxVLCPlugin2 vlc;
+        private List<string> _playList;
+        private Server _server;
+        private int _curIndex;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _curIndex = 0;
+
+            _playList = new List<string>();
+//            _playList.Add(@"D:\Videos\FTISLAND - Severely.mp4");
+//            _playList.Add(@"D:\Videos\miss A Bad Girl, Good Girl.mp4");
+//            _playList.Add(@"D:\Videos\T-ARA(티아라) _ Sexy Love (Dance Ver. MV).mp4");
+            _playList.Add(@"D:\Temp\o1928.mp3");
+            _playList.Add(@"D:\Temp\o115.mp3");
+
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _server = new Server();
+            Start();
+
+            vlc = new AxVLCPlugin2();
+            windowsFormsHost1.Child = vlc;
+        }
+
+        private void Start()
+        {
+            Thread startServer = new Thread(new ThreadStart(StartServer));
+            startServer.Name = "StartServer";
+            startServer.Start();
+        }
+
+        private void StartServer()
+        {
+            _server.Start();
+        }
+
+        private void UpdateContent()
+        {
+            if (_server.IsStarted)
+                _server.Stop();
+
+            if (_curIndex >= _playList.Count)
+                _curIndex = 0;
+            if (_curIndex < 0)
+                _curIndex = _playList.Count - 1;
+
+            string filename = _playList[_curIndex];
+
+            if (filename != string.Empty)
+            {
+                _server.FileName = filename;
+            }
+
+            vlc.playlist.add("http://localhost:7896/");
+            vlc.playlist.play();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            _curIndex = 0;
+            UpdateContent();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            vlc.playlist.stop();
+            _curIndex++;
+            UpdateContent();
+        }
+
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            vlc.playlist.stop();
+            _curIndex--;
+            UpdateContent();
+        }
+    }
+}
---
+++ vlcplayer/Server.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+
+using System.IO;
+using System.Threading.Tasks;
+
+namespace vlcplayer
+{ 
+    public class Server
+    {
+        private HttpListener _listener;
+        private string _filename = string.Empty;
+        private bool _isStop = false;
+        private bool _isStarted = false;
+        private Thread _requestThread;
+        private bool _isStopping = false;
+        private int _numberOfRequest;
+
+        private ManualResetEvent resetEvent;
+
+        public Server()
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://localhost:7896/");
+            _listener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
+            resetEvent = new ManualResetEvent(false);
+        }
+
+        private void RequestThread()
+        {
+            while (_listener.IsListening)
+            {
+                var context = _listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
+                context.AsyncWaitHandle.WaitOne();
+            }
+        }
+
+        private void ListenerCallback(IAsyncResult ar)
+        {
+            lock (this)
+            {
+                if (_isStopping)
+                    return;
+
+                resetEvent.Reset();
+                _numberOfRequest++;
+            }
+
+            var listener = ar.AsyncState as HttpListener;
+
+            System.Diagnostics.Debug.WriteLine(string.Format("ListenerCallback numberOfRequest = {0}", _numberOfRequest));
+
+            _isStarted = true;
+
+            var context = listener.EndGetContext(ar);
+
+            System.Threading.Tasks.Task.Factory.StartNew((ctx) =>
+            {
+                _isStop = false;
+                WriteFile((HttpListenerContext)ctx);
+            }, context, System.Threading.Tasks.TaskCreationOptions.LongRunning);
+
+            lock (this)
+            {
+                if (--_numberOfRequest == 0)
+                    resetEvent.Set();
+            }
+        }
+
+        public void Stop()
+        {
+            lock (this)
+            {
+                _isStopping = true;
+            }
+
+            resetEvent.WaitOne();
+            _isStopping = false;
+//            _listener.Stop();
+            System.Diagnostics.Debug.WriteLine("Listener Stopped");
+            _isStarted = false;
+        }
+
+        public void Start()
+        {
+            _listener.Start();
+            _listener.IgnoreWriteExceptions = true;
+
+            _requestThread = new Thread(RequestThread);
+            _requestThread.Name = "RequestThread";
+            _requestThread.Start();
+
+            _numberOfRequest = 0;
+        }
+/*
+        public void Start()
+        {
+            if (!HttpListener.IsSupported)
+                System.Diagnostics.Debug.WriteLine("Not supported");
+
+            _listener.Start();
+            _listener.IgnoreWriteExceptions = true;
+
+            _isStarted = true;
+
+
+            System.Threading.Tasks.Task.Factory.StartNew(() =>
+                {
+                   while (true)
+                    {
+                        HttpListenerContext context = _listener.GetContext();
+                        System.Threading.Tasks.Task.Factory.StartNew((ctx) =>
+                        {
+                            _isStop = false;
+                            WriteFile((HttpListenerContext)ctx);
+                        }, context, System.Threading.Tasks.TaskCreationOptions.LongRunning);
+                    }
+                }, System.Threading.Tasks.TaskCreationOptions.LongRunning);            
+        }
+
+        public void Stop()
+        {
+            _isStop = true;
+            _filename = string.Empty;
+        }
+*/
+        public bool IsStarted
+        {
+            get
+            {
+                return _isStarted;
+            }
+        }
+
+        void WriteFile(HttpListenerContext ctx)
+        {
+            if (FileName == string.Empty)
+                return;
+
+            var response = ctx.Response;
+
+            using (FileStream fs = File.OpenRead(FileName))
+            {
+                response.ContentLength64 = fs.Length;
+                //                response.SendChunked = false;
+                response.SendChunked = false;
+                response.ContentType = System.Net.Mime.MediaTypeNames.Application.Octet;
+                response.AddHeader("Cache-Control", "no-cache");
+                response.AddHeader("Content-Length", "0");
+                response.AddHeader("Content-disposition", "attachment; filename=1");
+
+//                byte[] buffer = new byte[64 * 1024];
+                byte[] buffer = new byte[64 * 1024];
+                int read;
+                int Count = 0;
+
+                using (BinaryWriter bw = new BinaryWriter(response.OutputStream))
+                {
+                    while ((read = fs.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        try
+                        {
+                            bw.Write(buffer, 0, read);
+                            bw.Flush();
+
+//                            System.Diagnostics.Debug.WriteLine(string.Format("total bytes = {0}", Count++));
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Diagnostics.Debug.WriteLine(string.Format("Exception - error = {0}", ex));
+                        }
+
+                        if (_isStop)
+                        {
+                            break;
+                        }
+                    }
+
+                    bw.Close();
+                }
+
+                response.StatusCode = (int)HttpStatusCode.OK;
+                response.StatusDescription = "OK";
+                response.OutputStream.Close();
+            }
+        }
+
+        public string FileName
+        {
+            set
+            {
+                if (value != string.Empty)
+                    _filename = value;
+            }
+            get
+            {
+                return _filename;
+            }
+        }
+    }
+}
